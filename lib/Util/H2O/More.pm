8491180ef7aacf726dd4fca5311586d0e9faaeab
Oodler 577 <oodler@cpan.org>
2021-12-10 23:40:07 -0600

    cleaned up POD

--- lib/Util/H2O/More.pm
+++ lib/Util/H2O/More.pm
@@ -49,10 +49,11 @@ sub baptise_deeply ($$@) {
 
 =head1 NAME
 
-Util::H2O::More - collection of handy Perl objects utilities
-using C<Util::H2O>'s C<h2o> utility. Chiefly, the idea is to
-provide a I<better> C<bless>, plus some other handy methods
-for performing precise surgery on blessed hash references.
+Util::H2O::More - collection of handy Perl B<pure hash reference>
+based objects utilities using C<Util::H2O>'s C<h2o> utility.
+Chiefly, the idea is to provide a I<better> C<bless>, plus some
+other handy methods for performing precise surgery on blessed
+hash references.
 
 =head1 SYNOPSIS
 
@@ -64,6 +65,7 @@ which means it includes accessors (thanks to C<Util::H2O::h2o>.
     
     package Foo::Bar;
 
+    # exports 'h2o' also
     use Util::H2O::More qw/baptise/;
      
     sub new {
@@ -82,7 +84,7 @@ Then on a client script,
      
     use Foo::Bar;
      
-    my $foo = Foo->new(some => q{thing});
+    my $foo = Foo::Bar->new(some => q{thing});
      
     print $foo->some . qq{\n};
      
@@ -97,20 +99,63 @@ The primary method, C<baptise>, essentially provides the same
 interface as the core keyword C<bless> with an additional third
 parameter where one may specify a list of default accessors.
 
+=head2 Why Was This Created?
+
+The short answer is, the original author wanted to maintain
+the usage pattern of C<bless>, but extend it to include the
+generation of accessors. He wanted a I<better bless> - one
+that took care of generating accessor methods automatically.
+
+The long answer...
+
+C<h2o> is an deceptively powerful tool that, above all, makes
+it I<easy> and I<fun> to add accessors to ad hoc hash references
+that many Perl developers like to use and that get emitted,
+I<unblessed> by many popular modules. For example, C<HTTP::Tiny>,
+C<Web::Scraper>, and the more common I<select%> methods C<DBI>
+flavors implement. 
+
+The usage pattern of C<h2o> begs it to be able to support being
+used as a I<drop in> replacement for C<bless>. However, this is
+not C<h2o>'s original intent and it will not work as a I<better
+bless>. But is does a fine job as serving as the I<basis> for a
+I<better bless>.
+
 =head1 METHODS
 
 =over 4
 
-=item C<baptise $href, $pkg, LIST>
+=item C<baptise $hash_ref, $pkg, LIST>
 
 Takes the same first 2 parameters as C<bless>; with the addition
 of a list that defines a set of default accessors that do not
 rely on the top level keys of the provided hash reference.
 
-=item C<baptise_deeply $href, $pkg, LIST>
+=item C<baptise_deeply $hash_ref, $pkg, LIST>
 
 Like C<baptise>, but creates accessors recursively for a nested
 hash reference. Uses C<h2o>'s C<-recurse> flag.
+
+Note: The accessors created in the nested hashes are handled
+directly by C<h2o> by utilizing the C<-recurse> flag. This means
+that they will necessarily be blessed using the unchangable
+behavior of C<h2o>, which maintains the name space of C<Util::H2O::_$hash>
+even if C<h2o> is passed with the C<-isa> and C<-class> flags,
+which are both utilized to achieve the effective outcome of
+C<baptise> and C<bastise_deeply>.
+
+
+=back
+
+=head1 EXTERNAL METHODS
+
+=over 4
+
+=item C<h2o>
+
+Because C<Util::H2O::More> exports C<h2o> as the basis for its
+operations, C<h2o> is also available without needing to qualify
+its fully qualified name space.
 
 =back
 
@@ -118,6 +163,10 @@ hash reference. Uses C<h2o>'s C<-recurse> flag.
 
 Requires C<Util::H2O> because this module is effectively a wrapper
 around C<h2o>.
+
+=head1 BUGS
+
+Yes, buyer beware.
 
 =head1 LICENSE AND COPYRIGHT 
 
